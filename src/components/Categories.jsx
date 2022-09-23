885546ff0d66e18d849fb5fde127a896c5258ea1
A1exZab <lisandrugrin@gmail.com>
2022-09-24 01:13:51 +0300

    Added 4 components

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -12,17 +12,13 @@ export default function Categories() {
 		'Закрытые'
 	]
 
-	const onClickCategory = (index) => {
-		setActiveIndex(index)
-	}
-
 	return (
 		<div className='categories'>
 			<ul>
 				{
 					categories.map(((category,index) => {
 						return(
-							<li onClick = {() => {onClickCategory(index)}} className = {activeIndex === index ? 'active' : ''}>{category}</li>
+							<li onClick = {() => {setActiveIndex(index)}} className = {activeIndex === index ? 'active' : ''}>{category}</li>
 						)
 					}))
 				}
