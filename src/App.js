b331ea5f0ad536718c9df6e1a792bcfbab9c12ce
A1exZab <lisandrugrin@gmail.com>
2022-09-25 21:39:48 +0300

    Added get request

--- src/App.js
+++ src/App.js
@@ -3,9 +3,22 @@ import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
-import pizzas from './assets/pizzas.json';
+import React from 'react';
+import axios from 'axios';
+import Skeleton from './components/PizzaBlock/Skeleton';
 
 function App() {
+	const itemsUrl = 'https://63303dc5591935f3c88c5806.mockapi.io/items';
+	const [items, setItems] = React.useState([]);
+	const [isLoading, setIsLoading] = React.useState(true);
+
+	React.useEffect(() => {
+		axios.get(itemsUrl).then((response) => {
+			setItems(response.data);
+			setIsLoading(false);
+		});
+	}, []);
+
 	return (
 		<div className='App'>
 			<div className='wrapper'>
@@ -18,9 +31,9 @@ function App() {
 						</div>
 						<h2 className='content__title'>Все пиццы</h2>
 						<div className='content__items'>
-							{pizzas.map((pizza) => {
-								return <PizzaBlock key={pizza.id} {...pizza} />;
-							})}
+							{isLoading
+								? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+								: items.map((pizza) => <PizzaBlock key={pizza.id} {...pizza} />)}
 						</div>
 					</div>
 				</div>
