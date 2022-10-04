ece7f806a5356d10f856a00f01a13957e253b04b
A1exZab <lisandrugrin@gmail.com>
2022-10-04 13:45:39 +0300

    Pagination added

--- src/App.js
+++ src/App.js
@@ -7,21 +7,23 @@ import NotFound from './pages/NotFound';
 import Cart from './pages/Cart';
 import './scss/app.scss';
 
+export const SearchContext = React.createContext();
+
 function App() {
+	const [searchValue, setSearchValue] = React.useState('');
+
 	return (
-		<div className='App'>
-			<div className='wrapper'>
+		<div className='wrapper'>
+			<SearchContext.Provider value={{ searchValue, setSearchValue }}>
 				<Header />
 				<div className='content'>
-					<div className='container'>
-						<Routes>
-							<Route path='/' element={<Home />} />
-							<Route path='/cart' element={<Cart />} />
-							<Route path='*' element={<NotFound />} />
-						</Routes>
-					</div>
+					<Routes>
+						<Route path='/' element={<Home />} />
+						<Route path='/cart' element={<Cart />} />
+						<Route path='*' element={<NotFound />} />
+					</Routes>
 				</div>
-			</div>
+			</SearchContext.Provider>
 		</div>
 	);
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,37 +1,48 @@
-import React from 'react'
-import axios from 'axios'
+import React from 'react';
+import axios from 'axios';
 
-import Categories from '../components/Categories'
-import Sort from '../components/Sort'
-import Skeleton from '../components/PizzaBlock/Skeleton'
-import PizzaBlock from '../components/PizzaBlock'
+import Categories from '../components/Categories';
+import Sort from '../components/Sort';
+import Skeleton from '../components/PizzaBlock/Skeleton';
+import PizzaBlock from '../components/PizzaBlock';
+import Pagination from '../Pagination';
+import { SearchContext } from '../App';
 
 export default function Home() {
-    const itemsUrl = 'https://63303dc5591935f3c88c5806.mockapi.io/items';
+	const { searchValue } = React.useContext(SearchContext);
+	const [categoryId, setCategoryId] = React.useState(0);
+	const [sortType, setSortType] = React.useState({
+		name: 'популярности',
+		sortProperty: 'rating',
+	});
+	const [currentPage, setCurrentPage] = React.useState(1);
+
 	const [items, setItems] = React.useState([]);
 	const [isLoading, setIsLoading] = React.useState(true);
+	const pizzas = items.map((pizza) => <PizzaBlock key={pizza.id} {...pizza} />);
+	const skeletons = [...new Array(6)].map((_, index) => <Skeleton key={index} />);
 
 	React.useEffect(() => {
+		const itemsUrl = `https://63303dc5591935f3c88c5806.mockapi.io/items?page=${currentPage}&limit=4&${
+			categoryId > 0 ? `category=${categoryId}` : ''
+		}&sortBy=${sortType.sortProperty}&order=desc&search=${searchValue}`;
+		setIsLoading(true);
 		axios.get(itemsUrl).then((response) => {
 			setItems(response.data);
 			setIsLoading(false);
 		});
-	}, []);  
+		window.scrollTo(0, 0);
+	}, [categoryId, sortType, searchValue, currentPage]);
 
-    return (
-    <>
-        <div className='content__top'>
-							<Categories />
-							<Sort />
-						</div>
-						<h2 className='content__title'>Все пиццы</h2>
-						<div className='content__items'>
-							{isLoading
-								? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-								: items.map((pizza) => <PizzaBlock key={pizza.id} {...pizza} />)}
-						</div>
-    </>
-  )
+	return (
+		<div className='container'>
+			<div className='content__top'>
+				<Categories value={categoryId} onChangeCategory={(index) => setCategoryId(index)} />
+				<Sort value={sortType} onChangeSort={(index) => setSortType(index)} />
+			</div>
+			<h2 className='content__title'>Все пиццы</h2>
+			<div className='content__items'>{isLoading ? skeletons : pizzas}</div>
+			<Pagination onChangePage={(pageNum) => setCurrentPage(pageNum)} />
+		</div>
+	);
 }
-
-
